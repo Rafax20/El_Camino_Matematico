475d432fe084109c2167ba6448c52859bac57caa
RafaX20 <cpadronrafael@gmail.com>
2026-07-01 06:09:37 -0400

    Cambiando cositas

--- Scripts_gd/menu.gd
+++ Scripts_gd/menu.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 func _on_boton_jugar_pressed():
-	# Cambia "res://tablero.tscn" por la ruta exacta de tu escena del tablero
 	get_tree().change_scene_to_file("res://Escenas/Tablero.tscn")
 
 func _on_boton_iniciar_sesion_pressed():
 	# En lugar de cambiar de escena, hacemos aparecer el Pop-up
-	$CanvasLayer/Ventana_Autenticacion.aparecer()
+	$CanvasLayer/Menu/Ventana_Autenticacion.aparecer()
+
+func _on_boton_logros_pressed():
+	get_tree().change_scene_to_file("res://Escenas/Album.tscn")
